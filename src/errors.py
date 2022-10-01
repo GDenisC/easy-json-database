d7e7e63757deb63b6f6fad9fc4f62c1cd7af2579
GDenisC <92527725+GDenisC@users.noreply.github.com>
2022-10-01 15:36:12 +0700

    Delete src directory

--- src/errors.py
+++
@@ -1,8 +0,0 @@
-
-__all__ = (
-    'DatabaseWrongCoding',
-)
-
-class DatabaseWrongCoding(Exception):
-    def __init__(self) -> None:
-        super().__init__("Encoder or decoder isn\'t equal type `Callable[[bytes], bytes]`")
